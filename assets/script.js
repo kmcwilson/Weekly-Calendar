7e40ba84a3eb6a9d1cc023eae3d9d998938aac04
Katie Wilson <kmcwilson4@gmail.com>
2022-09-15 12:55:38 -0400

    adding past, present and future

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
-let currentDate= moment().format('dddd, MMMM DD, YYYY');
+const currentDate= moment().format('dddd, MMMM DD, YYYY');
 $('#currentDay').text(currentDate);
+const timeofDay = moment().format('H');
+const hour = $('.row').children('.hour');
 
 function currentTime(){
-    
-}
+    if (hour < timeofDay){ 
+        $('.hour').addClass('past');
+    }
+    if (hour === timeofDay){
+        $('.hour').addClass('present');
+
+    }else{
+        $('.hour').addClass('future');
+    }
+   }
