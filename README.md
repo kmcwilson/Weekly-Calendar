ca96cbbe63a1097340f5b6aa67ba297bc4f6de5c
Katie Wilson <kmcwilson4@gmail.com>
2022-09-15 18:25:01 -0400

    added description to README file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# challenge-5-weekly-calendar
+# Weekly Calendar
+
+## Description
+
+Located in this repository is a weekly calendar that will show the current date. When the user looks through the calendar, it will be colour coded to past, present and future. Grey is a past event, present will be in red and the future is green. 
+Once the user types in their appointment for the day and clicks save, this will be stored on the page even if it is refreshed. If the user wants to delete the item, they can simply take it away and click save for the item to be taken off of their list. 
+
+## Screenshot
+
+
+## Link to live website
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,13 @@
+//adding the constant for currentDate to display the moment format for date
 const currentDate= moment().format('dddd, MMMM DD, YYYY');
+//text content for currentDay will now show the date from the moment format
 $('#currentDay').text(currentDate);
+//Using the moment format of hour in order to use for my each function
 const timeofDay = moment().format('H');
 const hours = $('.hour');
 const saveEvent= $('.saveBtn');
-
+//In this function we are looping through the attributes based on 
+//their ids that have been placed into an integer, if it is < = or > than the number within the moment format that will change its colour.
 hours.each (function (i){
   let time =  parseInt($(this).attr('id'));
   console.log(time);
@@ -16,9 +20,13 @@ hours.each (function (i){
     }else{
         $(this).next().addClass('future');
     }
+//local storage is now going to loop through and get the item based on its index, if not it will be an empty string
     $(this).next().children().text(localStorage.getItem(`todo ${i}`)|| '');
    });
 
+   //added an event listener to the function and this will go to each of the buttons 
+   //and target the value of its prev siblin's child element which is the text area. 
+   //This is then set into localstorge under the index of todo 0, todo 1 etc. 
 saveEvent.each (function(i){
     $(this).on('click', function(){
     let toDos=$(this).prev().children().val();
